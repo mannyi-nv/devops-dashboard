49f770c4d12b61b448ddf724298c2b236834348f
mannyi-nv <101787405+mannyi-nv@users.noreply.github.com>
2026-03-28 23:01:28 +0300

    Add logcheck function to inspect log files
    Implement log file inspection for errors

---
+++ logcheck.py
@@ -0,0 +1,25 @@
+def check_log():
+        #Docstring:
+    """
+    Inspect log file:
+        - file_log shopuld be provided by user input
+        - if 'ERROR:' is found in the log file, print the line containing 'ERROR:'
+        - if 'ERROR:' is not found, print a message indicating that no errors were found in the log file.
+    Note: The function should handle the case where the specified log file does not exist and prompt the user to try again.
+
+    """
+    while True:
+        try:
+            file_log = (input(f"\nPlease provide log file name: "))
+            with open(file_log, 'r') as log_file:
+                for line in log_file:
+                    if 'ERROR:' in line:
+                        print(line)
+                    else:
+                        print(f"\nNo 'ERROR' found in the log file.")
+        except FileNotFoundError:
+            print(f"\nFile '{file_log}' not found. Please try again.")
+            continue
+        
+
+        
