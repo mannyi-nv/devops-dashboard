3d384a086223f54869efc952566b973d374a6ddf
mannyi-nv <linuxsrh@gmail.com>
2026-03-28 23:54:52 +0300

    fix printing message

--- sysinfo.py
+++ sysinfo.py
@@ -13,19 +13,22 @@ def show_sysinfo():
         
     """
     while True:
-      try:
-          print(f"\nprint: hostname, current user, and current working directory")
-          hostname = socket.gethostname()
-          # 2. Get Current User
-          current_user = getpass.getuser()
-          # 3. Get Current Working Directory
-          cwd = os.getcwd()
+        try:
+            print(f"\nprinting: hostname, current user, and current working directory")
+            # 1. Get Hostname
+            hostname = socket.gethostname()
+            # 2. Get Current User
+            current_user = getpass.getuser()
+            # 3. Get Current Working Directory
+            cwd = os.getcwd()
   
-          print(f"Hostname: {hostname}")
-          print(f"Current User: {current_user}")
-          print(f"Current Working Directory: {cwd}")
-      except Exception as e:
-         print(f"Error! Something went wrong: {e}"
+            print(f"\n- Hostname: {hostname}")
+            print(f"- Current User: {current_user}")
+            print(f"- Current Working Directory: {cwd}")
+            break
+          
+        except Exception as e:
+         print(f"Error! Something went wrong: {e}")
          break
              
 
